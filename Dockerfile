af5efe3ab8bc21276d9616289edb851d888237c3
Alexandre Eher <alexandre.eher@werkspot.nl>
2016-04-25 23:07:17 +0200

    Enable swap using ssh

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM alpine
 ADD run.sh /
 ENV SWAPPINESS 10
 ENV SWAP_SIZE_IN_GB **None**
+VOLUME /user
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 if [ "${SWAP_SIZE_IN_GB}" != "**None**"  ]; then
     echo "=> Found swap size"
-    fallocate -l ${SWAP_SIZE_IN_GB}G /var/swap/${SWAP_SIZE_IN_GB}G
-    chmod 600 /var/swap/${SWAP_SIZE_IN_GB}G
-    mkswap /var/swap/${SWAP_SIZE_IN_GB}G
-    swapon /var/swap/${SWAP_SIZE_IN_GB}G
-    sysctl vm.swappiness=${SWAPPINESS}
+
+    echo "=> Creating ssh access"
+    mkdir -p /user/.ssh
+    chmod 700 /user/.ssh
+    ssh-keygen -Ndocker -f /users/.ssh/id_rsa
+    mv /users/.ssh/id_rsa.pub /users/.ssh/authorized_keys
+    chmod 600 /user/.ssh/authorized_keys
+
+    DOCKER_HOST=$(/sbin/ip route|awk '/default/ { print $3  }')
+    echo "=> Creating swap on ${DOCKER_HOST}"
+
+    ssh root@$DOCKER_HOST -i /users/.ssh/id_rsa fallocate -l ${SWAP_SIZE_IN_GB}G /swapfile
+    ssh root@$DOCKER_HOST -i /users/.ssh/id_rsa hmod 600 /swapfile
+    ssh root@$DOCKER_HOST -i /users/.ssh/id_rsa kswap /swapfile
+    ssh root@$DOCKER_HOST -i /users/.ssh/id_rsa wapon /swapfile
+
+    echo "=> Setting swappiness on ${DOCKER_HOST}"
+    ssh root@$DOCKER_HOST -i /users/.ssh/id_rsa ysctl vm.swappiness=${SWAPPINESS}
 else
     echo "ERROR: No swap size found in \$SWAP_SIZE_IN_GB"
     exit 1
