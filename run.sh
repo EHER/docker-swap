332eebc494ea57790c42ba7f23487915ccb29417
Alexandre Eher <alexandre.eher@werkspot.nl>
2016-04-25 22:16:16 +0200

    Change the swap file path

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ "${SWAP_SIZE_IN_GB}" != "**None**"  ]; then
     echo "=> Found swap size"
-    fallocate -l ${SWAP_SIZE_IN_GB}G /swapfile
-    chmod 600 /swapfile
-    mkswap /swapfile
-    swapon /swapfile
+    fallocate -l ${SWAP_SIZE_IN_GB}G /var/swap/${SWAP_SIZE_IN_GB}G
+    chmod 600 /var/swap/${SWAP_SIZE_IN_GB}G
+    mkswap /var/swap/${SWAP_SIZE_IN_GB}G
+    swapon /var/swap/${SWAP_SIZE_IN_GB}G
     sysctl vm.swappiness=${SWAPPINESS}
 else
     echo "ERROR: No swap size found in \$SWAP_SIZE_IN_GB"
